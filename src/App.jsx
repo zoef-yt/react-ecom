c9f7fd2bf27dcf615402493d92049cf37efe21bf
Zoef <shaikhzoef36@gmail.com>
2022-03-11 23:38:05 +0530

    Revert "Revert "Feat: Added products listing page to my e-com.""
    This reverts commit b3517d37126493571ff5e43b423ece20dede3727.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Homepage } from './components/homepage/homepage';
-
+// import { Homepage } from './components/homepage/homepage';
+import { ProductsPage } from './components/products-page/products-page';
 function App() {
-	return <Homepage />;
+	// return <Homepage />;
+	return <ProductsPage />;
 }
 
 export default App;
