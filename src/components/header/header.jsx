6cdf5a962aec8c4ba63a2542c673ab7b698bb1ca
Zoef <shaikhzoef36@gmail.com>
2022-03-11 19:07:36 +0530

    FEAT: Added theme to the app using useContext.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { ShoppingCartIcon, HeartIcon } from '../../assets/svg/svg';
+import { ShoppingCartIcon, HeartIcon, SunIcon, HalfMoonIcon } from '../../assets/svg/svg';
+import { useTheme } from '../../context/theme/theme-context';
 import '../css/header.css';
 const Header = () => {
+	const { theme, toggleTheme } = useTheme();
 	return (
 		<nav className='homepage-navbar'>
 			<div className='navbar-brand'>
-				<a href='eslint-bruh'>
-					{' '}
-					<h1>Need Games</h1>{' '}
+				<a href='/'>
+					<h1>Need Games</h1>
 				</a>
 			</div>
 			<input type='search' className='text-field' placeholder='Search For Games' />
 			<div className='navbar-cta'>
 				<HeartIcon className='wishlist-icon' />
 				<ShoppingCartIcon className='cart-icon' />
-				<a href='eslint-bruh' id='login-text' className='btn btn-link'>
+				<div onClick={toggleTheme}>{theme === 'dark' ? <SunIcon className='cart-icon' /> : <HalfMoonIcon className='cart-icon' />}</div>
+				<a href='eslint-bruh' className='btn btn-link'>
 					Hello, Login
 				</a>
 			</div>
