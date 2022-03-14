2251f0db098f53a909675d82c520c5eeb75f6b2d
Zoef <shaikhzoef36@gmail.com>
2022-03-14 13:42:39 +0530

    FEAT: Added mockbee to the app.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from './context/theme/theme-context';
+import { makeServer } from './server';
+
+// Call make Server
+makeServer();
 
 ReactDOM.render(
 	<React.StrictMode>
