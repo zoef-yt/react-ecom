fd2e8f8c285a2e589cacdb62d56125185ea44c4f
Zoef <shaikhzoef36@gmail.com>
2022-03-15 12:03:37 +0530

    Feat added wihslist page to ecom.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 // import { Homepage } from './components/homepage/homepage';
-import { ProductsPage } from './components/products-page/products-page';
+// import { ProductsPage } from './components/products-page/products-page';
+import { WishListPage } from './components/wishlist-page/wishlist-page';
+import '../src/components/css/homepage.css';
+import '../src/components/css/products-page.css';
 function App() {
+	return <WishListPage />;
 	// return <Homepage />;
-	return <ProductsPage />;
+	// return <ProductsPage />;
 }
 
 export default App;
